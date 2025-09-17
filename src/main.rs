e6fae016fe3dc5ac2e553927cf6c5bd3721be50f
Noah <bigspaceships56@gmail.com>
2025-09-17 09:09:29 -0400

    FEAT: getting json from data

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct RecordJson {
+    issue_datetime: String,
+    message: String,
+    product_id: String,
+}
+
+
+fn get_json() -> Result<String, reqwest::Error> {
+    let res =
+        reqwest::blocking::get("https://services.swpc.noaa.gov/products/alerts.json")?.text()?;
+
+    return Ok(res);
+}
+
+fn parse_json(text: &str) -> Result<Vec<RecordJson>, serde_json::Error> {
+    let v: Vec<RecordJson> = serde_json::from_str(text)?;
+
+    return Ok(v);
+}
+
 fn main() {
-    println!("Hello, world!");
+    match get_json() {
+        Ok(res) => {
+            let json = parse_json(&res);
+
+            match json {
+                Ok(v) => {
+                    println!("{:#?}", v)
+                }
+                Err(e) => {
+                    println!("{:?}", e)
+                }
+            }
+        }
+        Err(e) => {
+            println!("{:?}", e)
+        }
+    }
 }
