f0a8d5bde5623d5bed4d5f4e6c56cf487b9c6f7b
Noah <bigspaceships56@gmail.com>
2025-09-17 10:47:33 -0400

    FEAT: parse records with date and filter by day

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+use chrono::{DateTime, Days, FixedOffset, Utc};
 use serde::{Deserialize, Serialize};
+use anyhow::{Error, Result};
 
 #[derive(Serialize, Deserialize, Debug)]
 struct RecordJson {
@@ -7,33 +9,60 @@ struct RecordJson {
     product_id: String,
 }
 
+#[derive(Debug)]
+struct Record {
+    issue_datetime: DateTime<FixedOffset>,
+    message: String,
+    product_id: String,
+}
 
-fn get_json() -> Result<String, reqwest::Error> {
+impl Record {
+    fn from_json(json: RecordJson) -> Result<Record> {
+        println!("{}", json.issue_datetime);
+        let new_time_string = json.issue_datetime + "+0000";
+        let time = DateTime::parse_from_str(&new_time_string, "%Y-%m-%d %H:%M:%S%.3f %z")?;
+
+        return Ok(Record { issue_datetime: time, message: json.message, product_id: json.product_id })
+    }
+}
+
+fn get_json() -> Result<String> {
     let res =
         reqwest::blocking::get("https://services.swpc.noaa.gov/products/alerts.json")?.text()?;
 
     return Ok(res);
 }
 
-fn parse_json(text: &str) -> Result<Vec<RecordJson>, serde_json::Error> {
-    let v: Vec<RecordJson> = serde_json::from_str(text)?;
+fn parse_json(text: &str) -> Result<Vec<Record>> {
+    let v_json: Vec<RecordJson> = serde_json::from_str(text)?;
 
-    return Ok(v);
+    let v: Result<Vec<Record>> = v_json.into_iter().map(|r| Record::from_json(r)).collect();
+
+    return v;
+}
+
+fn filter_records(records: Vec<Record>) -> Vec<Record> {
+    let now: DateTime<Utc> = Utc::now();
+    let yesterday = now - Days::new(1);
+
+    return records.into_iter().filter(|record| record.issue_datetime > yesterday).collect();
 }
 
 fn main() {
     match get_json() {
         Ok(res) => {
-            let json = parse_json(&res);
+            let records = filter_records(parse_json(&res).unwrap());
 
-            match json {
-                Ok(v) => {
-                    println!("{:#?}", v)
-                }
-                Err(e) => {
-                    println!("{:?}", e)
-                }
-            }
+            println!("{:#?}", records);
+
+            // match records {
+            //     Ok(v) => {
+            //         println!("{:#?}", v)
+            //     }
+            //     Err(e) => {
+            //         println!("{:?}", e)
+            //     }
+            // }
         }
         Err(e) => {
             println!("{:?}", e)
